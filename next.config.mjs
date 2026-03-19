8b7026bd6c51c871459ebc33b5a9f6d1a32ba84f
Abhishek Sharma <abhisheksharma999r@gmail.com>
2026-03-19 23:29:29 +0530

    cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -9,6 +14,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  outputFileTracingRoot: __dirname,
 }
 
 export default nextConfig
